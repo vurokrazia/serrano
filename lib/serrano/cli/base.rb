0ab95d706eeb58d8e8754519d67110a3d16c106e
Jesús Martínez <jesus.alberto.vk@gmail.com>
2026-02-26 23:14:41 -0600

    Add orthogonal bootstrap flags for serrano new

--- lib/serrano/cli/base.rb
+++ lib/serrano/cli/base.rb
@@ -8,23 +8,31 @@ require_relative "generate"
 module Serrano
   module CLI
     class Base < Thor
+      DB_ADAPTER_GEMS = {
+        "sqlite" => "sqlite3",
+        "postgres" => "pg",
+        "mysql" => "mysql2"
+      }.freeze
+
       desc "new APP_NAME", "Create a new Serrano project"
       method_option :minimal, type: :boolean, default: false
+      method_option :db, type: :string, enum: DB_ADAPTER_GEMS.keys, banner: "sqlite|postgres|mysql"
       def new(app_name)
         root = File.expand_path(app_name, Dir.pwd)
         template_root = File.expand_path("templates", __dir__)
+        @db_adapter = options[:db]
+        @app_name = app_name
 
         FileUtils.mkdir_p(root)
         puts "create #{root}"
 
-        if options[:minimal]
-          write_template(template_root, "new_minimal_gemfile.tt", File.join(root, "Gemfile"))
-          write_template(template_root, "new_minimal_config.ru.tt", File.join(root, "config.ru"))
-        else
-          write_template(template_root, "new_default_gemfile.tt", File.join(root, "Gemfile"))
-          write_template(template_root, "new_default_config.ru.tt", File.join(root, "config.ru"))
-          write_template(template_root, "new_default_db.rb.tt", File.join(root, "config/db.rb"))
+        write_template(template_root, "new_project_gemfile.tt", File.join(root, "Gemfile"))
+        write_template(template_root, "new_project_config.ru.tt", File.join(root, "config.ru"))
+        write_template(template_root, "new_project_db.rb.tt", File.join(root, "config/db.rb")) if db_enabled?
 
+        if options[:minimal]
+          # minimal mode intentionally keeps only boot files (plus optional config/db.rb)
+        else
           %w[app/controllers app/services app/repositories app/entities db/migrations].each do |relative_dir|
             dir = File.join(root, relative_dir)
             FileUtils.mkdir_p(dir)
@@ -55,6 +63,29 @@ module Serrano
         rendered = ERB.new(template, trim_mode: "-").result(binding)
         File.write(destination, rendered)
         puts "create #{destination}"
+      end
+
+      def db_enabled?
+        !@db_adapter.nil?
+      end
+
+      def db_gem_name
+        DB_ADAPTER_GEMS[@db_adapter]
+      end
+
+      def config_db_require_line
+        db_enabled? ? "require_relative \"config/db\"" : nil
+      end
+
+      def db_connection_url
+        case @db_adapter
+        when "sqlite"
+          "sqlite://db/development.sqlite3"
+        when "postgres"
+          "postgres://localhost/#{@app_name}_development"
+        when "mysql"
+          "mysql2://root@localhost/#{@app_name}_development"
+        end
       end
     end
   end
